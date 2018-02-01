1e09c8e18cc93efd57b7971d4a630ff4bac5db98
Simas2006 <sjuknelis@gmail.com>
2018-01-31 19:14:28 -0500

    Add loading mechanism

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 var videoData,player1,player2;
 var index = 0;
+
+function onYouTubeIframeAPIReady() {
+  var req = new XMLHttpRequest();
+  req.open("GET","videos.json");
+  req.onload = function() {
+    videoData = JSON.parse(this.responseText);
+    player1 = new YT.Player("player1", {
+      videoId: videoData[index][0],
+      events: {
+        "onReady": onPlayerReady,
+        "onStateChange": onPlayerStateChange
+      }
+    });
+    player2 = new YT.Player("player2", {
+      videoId: videoData[index][1],
+      events: {
+        "onReady": onPlayerReady,
+        "onStateChange": onPlayerStateChange
+      }
+    });
+  }
+  req.send();
+}
+
+function onPlayerReady(event) {}
