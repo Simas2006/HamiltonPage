044e5cc835670fc0d22ff1da8248d34f11786a69
Simas2006 <sjuknelis@gmail.com>
2018-02-28 19:26:57 -0500

    Fully integrate expansion pack

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-var videoData,player1,player2;
+var videoData,resetData,player1,player2;
 var index = 0;
 var waiting = false;
+var expanded = false;
+
+function render() {
+  var animators = videoData.map(item => item[3].split(" (upload")[0]);
+  animators = animators.filter((item,index) => index == animators.indexOf(item));
+  animators = animators.map(item => `- ${item}\n`).join("");
+  document.getElementById("animators").innerText = "Thank you to our animators (in order of appearance): \n" + animators;
+  document.getElementById("author").innerText = videoData[index][2] + " by " + videoData[index][3];
+  var box = document.getElementById("song-dropdown");
+  while ( box.firstChild ) {
+    box.removeChild(box.firstChild);
+  }
+  for ( var i = 0; i < videoData.length; i++ ) {
+    var item = document.createElement("li");
+    var a = document.createElement("a");
+    a.innerText = videoData[i][2] + " by " + videoData[i][3];
+    a.href = "javascript: changeSong(" + i + ",true)";
+    item.appendChild(a);
+    box.appendChild(item);
+  }
+}
 
 function onYouTubeIframeAPIReady() {
   var req = new XMLHttpRequest();
   req.open("GET","videos.json");
   req.onload = function() {
     videoData = JSON.parse(this.responseText);
+    resetData = JSON.parse(this.responseText);
     player1 = new YT.Player("player1", {
       videoId: videoData[index][0],
       events: {
@@ -21,23 +43,13 @@ function onYouTubeIframeAPIReady() {
         "onStateChange": onPlayerStateChange
       }
     });
-    var animators = videoData.map(item => item[3].split(" (upload")[0]);
-    animators = animators.filter((item,index) => index == animators.indexOf(item));
-    animators = animators.map(item => `- ${item}\n`).join("");
-    document.getElementById("animators").innerText = "Thank you to our animators (in order of appearance): \n" + animators;
-    document.getElementById("author").innerText = videoData[index][2] + " by " + videoData[index][3];
-    var box = document.getElementById("song-dropdown");
-    for ( var i = 0; i < videoData.length; i++ ) {
-      var item = document.createElement("li");
-      var a = document.createElement("a");
-      a.innerText = videoData[i][2] + " by " + videoData[i][3];
-      a.href = "javascript: changeSong(" + i + ",true)";
-      item.appendChild(a);
-      box.appendChild(item);
-    }
-    setInterval(function() {
-      if ( Math.round(player1.getCurrentTime()) - videoData[index][4] != Math.round(player2.getCurrentTime()) && ! waiting ) player2.seekTo(player1.getCurrentTime() - videoData[index][4]);
-    },500);
+    videoData = videoData.filter(item => ! item[5]);
+    render();
+    setTimeout(function() {
+      setInterval(function() {
+        if ( Math.round(player1.getCurrentTime()) - videoData[index][4] != Math.round(player2.getCurrentTime()) && ! waiting ) player2.seekTo(player1.getCurrentTime() - videoData[index][4]);
+      },500);
+    },1000);
   }
   req.send();
 }
@@ -58,6 +70,21 @@ function onPlayerStateChange(event) {
   if ( event.data == YT.PlayerState.ENDED && event.target.a.id == "player1" ) {
     waiting = true;
   }
+}
+
+function toggleExpansion() {
+  expanded = ! expanded;
+  var newVideoData = resetData.map(item => item); // pointers
+  if ( ! expanded ) newVideoData = videoData.filter(item => ! item[5]);
+  if ( newVideoData.map(item => JSON.stringify(item)).indexOf(JSON.stringify(videoData[index])) == -1 ) {
+    index = newVideoData.map(item => JSON.stringify(item)).indexOf(JSON.stringify(videoData[index - 1])) + 1;
+    videoData = newVideoData;
+    changeSong(-1);
+  } else {
+    index = newVideoData.map(item => JSON.stringify(item)).indexOf(JSON.stringify(videoData[index]));
+    videoData = newVideoData;
+  }
+  render();
 }
 
 function changeSong(move,set) {
